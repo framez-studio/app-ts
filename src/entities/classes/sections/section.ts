2dad30620046e15dce210159fd6361a93009b7ba
raulps819 <raulps819@gmail.com>
2022-10-17 14:44:07 -0500

    Class RectangularHSection

--- src/entities/classes/sections/section.ts
+++ src/entities/classes/sections/section.ts
@@ -4,3 +4,18 @@ export abstract class Section implements ISection {
 	abstract get area(): number
 	abstract get inertiaZ(): number
 }
+
+export class RectangularHSection implements ISection{
+	area: number;
+	inertiaZ: number;
+	constructor(
+		public b: number,
+		public h: number,
+		public tb: number,
+		public th: number,
+		public outer_radius?: number,
+		public inner_radius?: number){
+			this.area = 0
+			this.inertiaZ = 0
+	}
+}
