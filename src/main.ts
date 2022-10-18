e267937c33bb65f6acac86064cf054256b2c8089
JP <cala.js@icloud.com>
2022-10-18 11:50:40 -0500

    Removed testing on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,1 @@
-import { SMatrix } from './entities/classes/matrices/s-matrix'
 import './style.css'
-
-let e, a, l, i
-e = 120
-a = 200
-l = 500
-i = 400
-
-let m = new SMatrix(e, l, a, i)
-// m.print()
