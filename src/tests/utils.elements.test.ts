6e7796c7ae2b1fd39c59e4eafc145c6268b9b69c
JP <cala.js@icloud.com>
2022-10-25 18:33:18 -0500

    UNFINISHED: assemblyMatrix function
    Renamed algebra test file

---
+++ src/tests/utils.elements.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from 'vitest'
+
+describe('Elements utils', () => {
+	it('should return a matrix with size according to the number of degs of freedom', () => {})
+	it.todo(
+		`should generate a matrix from a list of IElement's and INode's`,
+		() => {},
+	)
+})
--- src/utils/elements.ts
+++ src/utils/elements.ts
@@ -1,5 +1,16 @@
+import { MatrixOperator } from '../entities/classes/matrices/matrix-operator'
+import { IElement } from '../entities/interfaces/element.interface'
+import { INode } from '../entities/interfaces/nodes.interface'
+
+const MatOp = new MatrixOperator()
+
 export const uniques = <T>(...elements: T[]): T[] => {
 	let uniqueElements = new Set()
 	elements.forEach((element) => uniqueElements.add(element))
 	return Array.from(uniqueElements) as T[]
+}
+
+export const assemblyMatrix = (nodes: INode[], elements: IElement[]) => {
+	let degs = nodes.length
+	return
 }
