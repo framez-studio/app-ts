33563f3315c4bbf8600d69979477451a96d57cb9
JP <cala.js@icloud.com>
2022-10-27 18:07:48 -0500

    Removed unused entities

--- src/entities/classes/nodes/fixed-support.ts
+++
@@ -1,8 +0,0 @@
-import { INode, ISupport } from '../../interfaces/nodes.interface'
-import { Support } from './support'
-
-export class FixedSupport extends Support implements ISupport {
-	constructor(node: INode) {
-		super(node, { dx: true, dy: true, rz: true })
-	}
-}
