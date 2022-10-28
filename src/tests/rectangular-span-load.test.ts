87e8eddbccf93be661955a40e8a6f4a0dfbce915
JP <cala.js@icloud.com>
2022-10-28 14:49:11 -0500

    Added sum of forces tests

--- src/tests/rectangular-span-load.test.ts
+++ src/tests/rectangular-span-load.test.ts
@@ -4,12 +4,35 @@ import { RectangularSpanLoad } from '../entities/classes/others/rectangular-span
 describe('Rectangular Span Load Class', () => {
 	const w = 20
 	const l = 2
-	const load = new RectangularSpanLoad(w, l)
+	let a = 0
+	let b = l
+	const load = new RectangularSpanLoad(w, l, a, l)
 	it('should calculate correctly the FEF on a full span load', () => {
 		let result = load.fef
-		expect(result.initial.mz).toBe((w * l ** 2) / 12)
-		expect(result.final.mz).toBe(-(w * l ** 2) / 12)
+		expect(result.initial.fx).toBe(0)
 		expect(result.initial.fy).toBe((w * l) / 2)
+		expect(result.initial.mz).toBe((w * l ** 2) / 12)
+		expect(result.final.fx).toBe(0)
 		expect(result.final.fy).toBe((w * l) / 2)
+		expect(result.final.mz).toBe(-(w * l ** 2) / 12)
+	})
+	it('should provide x-forces equilibrium', () => {
+		let result = load.fef
+		let sum = result.initial.fx + result.final.fx
+		expect(sum).toBe(0)
+	})
+	it('should provide y-forces equilibrium', () => {
+		let result = load.fef
+		let sum = result.initial.fy + result.final.fy - w * (b - a)
+		expect(sum).toBe(0)
+	})
+	it('should provide momentum equilibrium', () => {
+		let result = load.fef
+		let sum =
+			result.initial.mz +
+			result.final.mz +
+			result.final.fy * l -
+			(w * (b - a) ** 2) / 2
+		expect(sum).toBe(0)
 	})
 })
