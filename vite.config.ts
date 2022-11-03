987945293bb1e4b2ad23157338f00a76d87ec8d9
JP <cala.js@icloud.com>
2022-11-03 18:22:36 -0500

    Path alias added

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,31 @@
+import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+	resolve: {
+		alias: [
+			{
+				find: '@',
+				replacement: path.resolve(__dirname, 'src'),
+			},
+			{
+				find: '@interfaces',
+				replacement: path.resolve(__dirname, 'src/entities/interfaces'),
+			},
+			{
+				find: '@classes',
+				replacement: path.resolve(__dirname, 'src/entities/classes'),
+			},
+			{
+				find: '@types',
+				replacement: path.resolve(__dirname, 'src/entities/types'),
+			},
+			{
+				find: '@utils',
+				replacement: path.resolve(__dirname, 'src/utils'),
+			},
+		],
+	},
 	test: {
 		coverage: {
 			reporter: ['text'],
