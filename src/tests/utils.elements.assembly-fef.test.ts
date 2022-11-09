12c8584357602b8d77a19073e2dc73c0ac7d7bdd
JP <cala.js@icloud.com>
2022-11-09 16:18:02 -0500

    assemblyFef test implemented and passed

--- src/tests/utils.elements.assembly-fef.test.ts
+++ src/tests/utils.elements.assembly-fef.test.ts
@@ -21,8 +21,7 @@ describe(`FEF Assembly function`, () => {
 	it('should generate an assembled fef vector correctly for only a one force', () => {
 		let w = 20
 		let l = beam.length
-		let load = new RectangularSpanLoad(w, l)
-		beam.addSpanLoad(load)
+		beam.addSpanLoad(new RectangularSpanLoad(w, l))
 		let expected = [
 			[0],
 			[0],
@@ -40,26 +39,30 @@ describe(`FEF Assembly function`, () => {
 		let result = assemblyFef([a, b, c, d], [beam, lCol, rCol])
 		expect(result).toEqual(expected)
 	})
-	it('should generate an assembled fef vector correctly for two forces', () => {
+	it('should generate fefs on global coordinates for rotated elements', () => {
 		let w = 20
 		let l = beam.length
-		let load = new RectangularSpanLoad(w, l)
-		beam.addSpanLoad(load)
+		let lc = lCol.length
+		lCol.addSpanLoad(new RectangularSpanLoad(w, lc))
 		let expected = [
+			[-(w * lc) / 2],
 			[0],
+			[(w * lc ** 2) / 12],
+			[-(w * lc) / 2 + 0],
+			[0 + (w * l) / 2],
+			[-(w * lc ** 2) / 12 + (w * l ** 2) / 12],
 			[0],
-			[0],
-			[0],
-			[(2 * (w * l)) / 2],
-			[(2 * (w * l ** 2)) / 12],
-			[0],
-			[(2 * (w * l)) / 2],
-			[(-2 * (w * l ** 2)) / 12],
+			[(w * l) / 2],
+			[-(w * l ** 2) / 12],
 			[0],
 			[0],
 			[0],
 		]
 		let result = assemblyFef([a, b, c, d], [beam, lCol, rCol])
-		expect(result).toEqual(expected)
+		result.forEach((row, i) => {
+			row.forEach((value, j) => {
+				expect(value).toBeCloseTo(expected[i][j])
+			})
+		})
 	})
 })
