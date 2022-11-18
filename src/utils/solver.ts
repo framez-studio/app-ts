16562233dcec2e3e7fc3b9bc4edfc4bd23982a68
JP <cala.js@icloud.com>
2022-11-18 11:05:26 -0500

    Typo error

--- src/utils/solver.ts
+++ src/utils/solver.ts
@@ -5,7 +5,7 @@ import { MatrixOperator as MatOp } from '@classes'
  * Calculates the linear expression a*x = b.
  * @param a - nxn coefficient matrix
  * @param b - nx1 independent values vector
- * @returns An nx1 vector containing the solution of the system, the 'x' vector.
+ * @returns A nx1 vector containing the solution of the system, the 'x' vector.
  */
 export const solveLinearSystem = (a: Array2D, b: Array2D): Array2D => {
 	let inverseCoefMatrix = MatOp.inverse(a)
