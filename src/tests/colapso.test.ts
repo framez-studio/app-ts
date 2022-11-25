2991d65289663c1f0b50d9e411ddcbc046b1fcff
JP <cala.js@icloud.com>
2022-11-25 15:02:03 -0500

    Refactored to match new interfaces

--- src/tests/colapso.test.ts
+++ src/tests/colapso.test.ts
@@ -11,19 +11,19 @@ import { Concrete21 } from '@/utils/material'
 
 describe('xxxx', () => {
 	// structure definition
-	const col350x350 = new RectangularSectionCR(0.35,0.35,Concrete21)
-    const vga400x300 = new RectangularSectionCR(0.4,0.3,Concrete21)
+	const col350x350 = new RectangularSectionCR(0.35, 0.35, Concrete21)
+	const vga400x300 = new RectangularSectionCR(0.4, 0.3, Concrete21)
 	let e = 26587215
-	let a = new Support('fixed', 0, 0)
-	let b = new ElementNode(0, 3)
-	let c = new ElementNode(6, 3)
-	let d = new Support('fixed', 6, 0)
+	let a = new Support('fixed', { x: 0, y: 0 })
+	let b = new ElementNode({ x: 0, y: 3 })
+	let c = new ElementNode({ x: 6, y: 3 })
+	let d = new Support('fixed', { x: 6, y: 0 })
 	let lCol = new Element(a, b, col350x350, e)
 	let beam = new Element(b, c, vga400x300, e)
 	let rCol = new Element(c, d, col350x350, e)
-    b.addLoad("fx",1)
+	b.addLoads({ fx: 1 })
 	const structure = new Structure(lCol, beam, rCol)
-    it(`xxxxx`, () => {
+	it(`xxxxx`, () => {
 		let expected = [
 			[0],
 			[0],
@@ -38,10 +38,10 @@ describe('xxxx', () => {
 			[0],
 			[0],
 		]
-		let result = structure.displacements('array')
+		let result = structure.displacements
 		result.forEach((row, i) => {
 			row.forEach((value, j) => {
-				expect(value).toBeCloseTo(expected[i][j],4)
+				expect(value).toBeCloseTo(expected[i][j], 4)
 			})
 		})
 	})
