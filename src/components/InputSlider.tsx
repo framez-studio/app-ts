c25d96edbfd4db399c5e811fd5740b9a5ac80af3
JP <cala.js@icloud.com>
2023-01-01 17:49:06 -0500

    SliderInput header created

--- src/components/InputSlider.tsx
+++ src/components/InputSlider.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import '@styles/Slider.sass'
 import '@styles/InputSlider.sass'
+import InputSliderSlot from './InputSliderSlot'
 
 const InputSlider = () => {
-	return <section className="slider input-slider">InputSlider</section>
+	return (
+		<section className="slider input-slider">
+			<InputSliderSlot props={{ isOpen: false }} />
+		</section>
+	)
 }
 
 export default InputSlider
---
+++ src/components/InputSliderSlot.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import '@styles/InputSliderSlot.sass'
+import ArrowDownwardBtn from './ArrowDownwardBtn'
+import ArrowUpwardBtn from './ArrowUpwardBtn'
+
+interface Props extends React.HTMLProps<HTMLDivElement> {
+	props: {
+		isOpen: boolean
+	}
+}
+
+const InputSliderSlot: React.FC<Props> = ({ props }) => {
+	const [isOpen, setIsOpen] = useState(props.isOpen ?? false)
+	const clickHandler = () => {
+		setIsOpen(!isOpen)
+	}
+	return (
+		<section className="slider-slot">
+			{isOpen ? (
+				<ArrowDownwardBtn props={{ onClick: clickHandler }} />
+			) : (
+				<ArrowUpwardBtn props={{ onClick: clickHandler }} />
+			)}
+		</section>
+	)
+}
+
+export default InputSliderSlot
